3984aa8dda8a11a99103d4652f037fab5e62212c
albert.sigman <albert.sigman@zenecosystems.com>
2022-04-11 17:47:46 -0400

    updated chart

--- energyBreakdown/summaryChart.js
+++ energyBreakdown/summaryChart.js
@@ -1,3 +1,17 @@
+const getXWidth = (d, xz, type, isGranular) => {
+  let len = isGranular
+    ? d3.timeHour.offset(d.timeset, 1)
+    : d3.timeDay.offset(d.timeset, 1);
+
+  len = (xz(len) - xz(d.timeset)) * 0.6;
+  if (type === "x") {
+    return xz(d.timeset) - len / 2;
+  }
+  if (type === "width") {
+    return len;
+  }
+};
+
 // http://bl.ocks.org/adg29/8868399
 const getGranular = (x) => {
   const earliest = new Date(x.domain()[0]);
@@ -10,19 +24,30 @@ const getGranular = (x) => {
   };
   return {
     isGranular:
-      da.earlyMonth === da.latestMonth && da.latestDay - da.earlyDay <= 3,
+      da.earlyMonth === da.latestMonth && da.latestDay - da.earlyDay <= 4,
     diff: da.latestDay - da.earlyDay,
     ...da,
   };
 };
 export const generateSummaryChart = () => {
-  var svg = d3.select("svg.energy-breakdown"),
+  var svg = d3.select("svg.energy-summary"),
     margin = { top: 30, right: 50, bottom: 30, left: 50 },
     tooltipHeight = 80,
     tooltipWidth = svg.attr("width") - margin.left - margin.right,
     width = +svg.attr("width") - margin.left - margin.right,
     height = +svg.attr("height") - margin.top - margin.bottom - tooltipHeight;
   svg.attr("viewBox", [0, 0, width, height]);
+  svg.style("cursor", "crosshair");
+
+  const tooltipDiv = d3
+    .select(".energy-summary--tooltip")
+    .style("top", `${height + tooltipHeight}px`)
+    .style("left", "0");
+
+  const toolTipDateDiv = tooltipDiv.select("#energy-summary--tooltip__date");
+  const toolTipUsageDiv = tooltipDiv.select("#energy-summary--tooltip__usage");
+
+  const hourFormat = d3.timeFormat("%-I%p");
 
   function render(data, threshold) {
     // Transform data
@@ -72,10 +97,9 @@ export const generateSummaryChart = () => {
           .axisBottom()
           .scale(x)
           .tickSize(5)
-          .ticks(isGranular ? d3.timeMinute.every(60) : 10)
+          .ticks(isGranular ? d3.timeHour.every(2) : 10)
           .tickFormat((d, i) => {
             if (isGranular) {
-              const hourFormat = d3.timeFormat("%-I%p");
               return hourFormat(d);
             }
             const f = d3.timeFormat("%-m/%d");
@@ -138,25 +162,64 @@ export const generateSummaryChart = () => {
     const gx = svg.append("g").call(xAxis, x);
     const gy = svg.append("g").call(yAxis, y);
 
-    const bars = svg
-      .append("g")
-      .attr("class", "data-bars")
-      .selectAll("rect")
+    const barsG = svg.append("g").attr("class", "data-bars");
+
+    const bars = barsG
+      .selectAll("rolled-rect")
       .data(rolledData)
       .enter()
-      .append("rect");
+      .append("rect")
+      .on("mouseover", function (event, d) {
+        d3.select(this).attr("opacity", 0.9);
+        tooltipDiv
+          .transition()
+          .duration(500)
+          .style("opacity", 1)
+          .style("left", `${event.pageX - 120}px`);
 
-    const granBars = svg
-      .append("g")
-      .attr("class", "data-bars")
-      .selectAll("rect")
+        toolTipDateDiv.text(() => {
+          const f = d3.timeFormat("%m/%d");
+          return f(d.timeset);
+        });
+
+        toolTipUsageDiv.text(`${d.usage} kwh`);
+      })
+      .on("mouseout", function (event, d) {
+        d3.select(this).attr("opacity", 1);
+      });
+
+    const granBarsG = svg.append("g").attr("class", "granular-data-bars");
+
+    const granBars = granBarsG
+      .selectAll("granular-rect")
+      .data(data)
+      .enter()
+      .append("rect")
+      .on("mouseover", function (event, d) {
+        d3.select(this).attr("opacity", 0.9);
+        tooltipDiv
+          .transition()
+          .duration(500)
+          .style("opacity", 1)
+          .style("left", `${event.pageX - 120}px`);
+
+        toolTipDateDiv.text(() => hourFormat(d.timeset));
+
+        toolTipUsageDiv.text(`${d.usage} kwh`);
+      })
+      .on("mouseout", function (event, d) {
+        d3.select(this).attr("opacity", 1);
+      });
+
+    const threshBar = granBarsG
+      .selectAll("above-rect")
       .data(data)
       .enter()
       .append("rect");
 
     const zoom = d3
       .zoom()
-      .scaleExtent([1, data.length])
+      .scaleExtent([0.9, data.length / 20])
       .extent([
         [margin.left, 0],
         [width - margin.right, height],
@@ -165,7 +228,6 @@ export const generateSummaryChart = () => {
         [margin.left, -Infinity],
         [width - margin.right, Infinity],
       ])
-
       .on("zoom", zoomed);
 
     function zoomed(event) {
@@ -178,60 +240,40 @@ export const generateSummaryChart = () => {
       mx.call(x1MonthAxis, xz);
       gx.call(xAxis, xz);
       gy.call(yAxis, y);
-      const { earlyMonth, latestMonth } = {
-        earlyMonth: Date.parse(new Date(xz.domain()[0])),
-        latestMonth: Date.parse(new Date(xz.domain()[1])),
-      };
-
-      const datamin = data.findIndex((d) => d.dateNum > earlyMonth);
-      const datamax = data.findIndex((d) => d.dateNum > latestMonth);
 
       if (isGranular) {
         // gy.call(yAxis, y);
         // svg.selectAll("rect").exit().remove(bars);
         bars.attr("fill", "none");
         granBars
-          .attr("x", (d) => {
-            let len = d3.timeHour.offset(d.timeset, 1);
-            // console.log(x(next));
-            len = xz(len) - xz(d.timeset);
-            len = len * 0.6;
-            return xz(d.timeset) - len / 2;
-          })
+          .attr("x", (d) => getXWidth(d, xz, "x", isGranular))
           .attr("y", (d) => y(d.usage) + height - margin.bottom)
-          .attr("width", (d) => {
-            let len = d3.timeHour.offset(d.timeset, 1);
-            // console.log(x(next));
-            len = xz(len) - xz(d.timeset);
-            len = len * 0.6;
-            return len;
-          })
-          .attr("fill", (d) =>
-            d.usage > threshold.hourly
-              ? "var(--zen-warning)"
-              : "var(--zen-blue)",
-          )
+          .attr("width", (d) => getXWidth(d, xz, "width", isGranular))
+          .attr("fill", "var(--zen-blue)")
           .attr("height", (d) => -y(d.usage));
+
+        threshBar
+          .attr("fill", "var(--zen-warning)")
+          .attr("height", (d) => {
+            if (d.usage > threshold.hourly) {
+              return -y(d.usage) + y(threshold.hourly);
+            } else {
+              return 0;
+            }
+          })
+          .attr("width", (d) => getXWidth(d, xz, "width", isGranular))
+          .attr("x", (d) => getXWidth(d, xz, "x", isGranular))
+          .attr("y", (d) => y(d.usage) + height - margin.bottom);
       } else {
         // gy.call(yAxis, y);
+
+        threshBar.attr("fill", "none");
         granBars.attr("fill", "none");
         bars
           .attr("height", (d) => -y(d.usage))
-          .attr("width", (d) => {
-            let len = d3.timeDay.offset(d.timeset, 1);
-            // console.log(x(next));
-            len = xz(len) - xz(d.timeset);
-            len = len * 0.6;
-            return len;
-          })
+          .attr("width", (d) => getXWidth(d, xz, "width", isGranular))
           .attr("y", (d) => y(d.usage) + height - margin.bottom)
-          .attr("x", (d) => {
-            let len = d3.timeDay.every(1).offset(d.timeset, 1);
-            // console.log(x(next));
-            len = xz(len) - xz(d.timeset);
-            len = len * 0.6;
-            return xz(d.timeset) - len / 2;
-          })
+          .attr("x", (d) => getXWidth(d, xz, "x", isGranular))
           .attr("fill", (d) =>
             d.usage > threshold.daily
               ? "var(--zen-warning)"
@@ -244,7 +286,8 @@ export const generateSummaryChart = () => {
       .call(zoom)
       .transition()
       .duration(750)
-      .call(zoom.scaleTo, 4, [x(data[data.length - 1].timeset), 0]);
+      .call(zoom.scaleTo, 4, [x(data[data.length - 1].timeset), 0])
+      .call(d3.drag());
   }
 
   d3.json("../data/energyUsage.json").then((d) => render(d.data, d.threshold));
