adf2587d897514e1ef198835a5f1be4087bb7531
albert.sigman <albert.sigman@zenecosystems.com>
2022-05-11 19:53:57 -0400

    testing

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '6a1a0508cb2a3b44d7bcc5ffddebd0e5deddf5e5',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '6a1a0508cb2a3b44d7bcc5ffddebd0e5deddf5e5',
+            'dev_requirement' => false,
+        ),
+    ),
+);
